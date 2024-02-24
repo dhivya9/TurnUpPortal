cf84e31b1a8a328fb9649eea89bb7e074652667e
Dhivya Gopal <dhivyatech23@gmail.com>
2024-02-25 01:18:22 +1100

    add this login function

---
+++ Login Function for turnup portal/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+
+PageObjectModel loginpages = new PageObjectModel();
+
+internal class PageObjectModel
+{
+}
+
+IWebElement username = driver.FindElement(By.Id("UserName"));
+username.SendKeys("hari");
+IWebElement password=driver.FindElement(By.Id("Password"));
+password.SendKeys("123123");
+IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginbutton.Click();
+IWebElement loginpage = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if (loginpage != null) 
+{
+    Console.WriteLine("user logeed in successfully");
+}
+else
+    {
+    Console.WriteLine("user are logged in properly");
+}
